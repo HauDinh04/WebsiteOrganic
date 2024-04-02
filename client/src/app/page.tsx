ef0c7beca5bb878010043aa6f06fd049567def7c
tatuan2907 <tatuan290703@gmail.com>
2024-04-02 11:52:16 +0700

    sua trang home

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -40,29 +40,45 @@ export default function Home() {
             <p className="w-[440px] h-[50px] mx-auto text-center text-black text-[40px] font-bold font-['Inter']">Sản phẩm bán chạy</p>
           </div>
           <div className="w-[1200px] h-[360px] flex">
-            <div className="w-60 h-[360px] pt-[10px]">
-              <Image src={"/images/products/product1.png"} width={240} height={240} alt="product"></Image>
-              <p className="w-[60px] h-5 mt-[10px] mx-auto text-black text-base font-normal font-['Inter']">Bắp cải</p>
-              <p className="w-[60px] h-5 mx-auto mt-[10px] text-lime-600 text-base font-bold font-['Inika']">65,000 <span className="underline">đ</span></p>
-              <div className="w-[110px] h-[30px] mx-auto mt-[10px]"><button className="w-[110px] h-[30px]  bg-lime-600 rounded-[5px] shadow text-center text-neutral-100 font-normal font-['Poppins']">Thêm giỏ hàng</button></div>
+            <div className="w-60 h-[360px] mb-10 relative">
+              <Image src={"/images/products/product2.png"} width={240} height={276} alt="product"></Image>
+              <p className="h-[20px] text-center text-black text-base font-bold font-['Inter'] absolute bottom-[100px] left-[80px]">Rau Súp Lơ</p>
+              <div className="h-[84px] flex items-center justify-around">
+                <p className="w-[82px] h-[21px] text-center text-black text-sm font-semibold font-['Inter']">38.000 vnđ</p>
+                <button className="w-[83px] h-9 bg-lime-600 rounded-[5px] text-center text-white text-xs font-semibold font-['Inter']">Mua Ngay</button>
+              </div>
             </div>
-            <div className="w-60 h-[360px] pt-[10px]">
-              <Image src={"/images/products/product1.png"} width={240} height={240} alt="product"></Image>
-              <p className="w-[60px] h-5 mt-[10px] mx-auto text-black text-base font-normal font-['Inter']">Bắp cải</p>
-              <p className="w-[60px] h-5 mx-auto mt-[10px] text-lime-600 text-base font-bold font-['Inika']">65,000 <span className="underline">đ</span></p>
-              <div className="w-[110px] h-[30px] mx-auto mt-[10px]"><button className="w-[110px] h-[30px]  bg-lime-600 rounded-[5px] shadow text-center text-neutral-100 font-normal font-['Poppins']">Thêm giỏ hàng</button></div>
+            <div className="w-60 h-[360px] mb-10 relative">
+              <Image src={"/images/products/product2.png"} width={240} height={276} alt="product"></Image>
+              <p className="h-[20px] text-center text-black text-base font-bold font-['Inter'] absolute bottom-[100px] left-[80px]">Rau Súp Lơ</p>
+              <div className="h-[84px] flex items-center justify-around">
+                <p className="w-[82px] h-[21px] text-center text-black text-sm font-semibold font-['Inter']">38.000 vnđ</p>
+                <button className="w-[83px] h-9 bg-lime-600 rounded-[5px] text-center text-white text-xs font-semibold font-['Inter']">Mua Ngay</button>
+              </div>
             </div>
-            <div className="w-60 h-[360px] pt-[10px]">
-              <Image src={"/images/products/product1.png"} width={240} height={240} alt="product"></Image>
-              <p className="w-[60px] h-5 mt-[10px] mx-auto text-black text-base font-normal font-['Inter']">Bắp cải</p>
-              <p className="w-[60px] h-5 mx-auto mt-[10px] text-lime-600 text-base font-bold font-['Inika']">65,000 <span className="underline">đ</span></p>
-              <div className="w-[110px] h-[30px] mx-auto mt-[10px]"><button className="w-[110px] h-[30px]  bg-lime-600 rounded-[5px] shadow text-center text-neutral-100 font-normal font-['Poppins']">Thêm giỏ hàng</button></div>
+            <div className="w-60 h-[360px] mb-10 relative">
+              <Image src={"/images/products/product2.png"} width={240} height={276} alt="product"></Image>
+              <p className="h-[20px] text-center text-black text-base font-bold font-['Inter'] absolute bottom-[100px] left-[80px]">Rau Súp Lơ</p>
+              <div className="h-[84px] flex items-center justify-around">
+                <p className="w-[82px] h-[21px] text-center text-black text-sm font-semibold font-['Inter']">38.000 vnđ</p>
+                <button className="w-[83px] h-9 bg-lime-600 rounded-[5px] text-center text-white text-xs font-semibold font-['Inter']">Mua Ngay</button>
+              </div>
             </div>
-            <div className="w-60 h-[360px] pt-[10px]">
-              <Image src={"/images/products/product1.png"} width={240} height={240} alt="product"></Image>
-              <p className="w-[60px] h-5 mt-[10px] mx-auto text-black text-base font-normal font-['Inter']">Bắp cải</p>
-              <p className="w-[60px] h-5 mx-auto mt-[10px] text-lime-600 text-base font-bold font-['Inika']">65,000 <span className="underline">đ</span></p>
-              <div className="w-[110px] h-[30px] mx-auto mt-[10px]"><button className="w-[110px] h-[30px]  bg-lime-600 rounded-[5px] shadow text-center text-neutral-100 font-normal font-['Poppins']">Thêm giỏ hàng</button></div>
+            <div className="w-60 h-[360px] mb-10 relative">
+              <Image src={"/images/products/product2.png"} width={240} height={276} alt="product"></Image>
+              <p className="h-[20px] text-center text-black text-base font-bold font-['Inter'] absolute bottom-[100px] left-[80px]">Rau Súp Lơ</p>
+              <div className="h-[84px] flex items-center justify-around">
+                <p className="w-[82px] h-[21px] text-center text-black text-sm font-semibold font-['Inter']">38.000 vnđ</p>
+                <button className="w-[83px] h-9 bg-lime-600 rounded-[5px] text-center text-white text-xs font-semibold font-['Inter']">Mua Ngay</button>
+              </div>
+            </div>
+            <div className="w-60 h-[360px] mb-10 relative">
+              <Image src={"/images/products/product2.png"} width={240} height={276} alt="product"></Image>
+              <p className="h-[20px] text-center text-black text-base font-bold font-['Inter'] absolute bottom-[100px] left-[80px]">Rau Súp Lơ</p>
+              <div className="h-[84px] flex items-center justify-around">
+                <p className="w-[82px] h-[21px] text-center text-black text-sm font-semibold font-['Inter']">38.000 vnđ</p>
+                <button className="w-[83px] h-9 bg-lime-600 rounded-[5px] text-center text-white text-xs font-semibold font-['Inter']">Mua Ngay</button>
+              </div>
             </div>
           </div>
         </div>
