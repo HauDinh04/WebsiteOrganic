98be21c220b0f346d9ef9916ffe1e87ed2d507e5
minh le <117999133+sixxson@users.noreply.github.com>
2024-04-08 00:02:36 +0700

    task layout about

--- client/src/app/about/page.tsx
+++ client/src/app/about/page.tsx
@@ -1,7 +1,70 @@
+import Image from "next/image";
+
 const About = () => {
-    return (  
-        <div className="">About Pages</div>
+    return (
+        <div className="max-w-[1200px] relative">
+            <div className="block w-[1200px] h-[100px]">
+                <h1 className="absolute z-[1] top-[25px] left-[45%] text-white">Home / About us</h1>
+                <Image className="absolute z-0"
+                    src={"/images/breadcrumb.png"}
+                    alt=""
+                    width={1200}
+                    height={100}
+                />
+            </div>
+            <div className="">
+                <div className="flex flex-col justify-center items-center">
+                    <h1 className="
+                    text-3xl
+                    font-semibold
+                    text-center">
+                        What we do?
+                    </h1>
+                    <p className="
+                    w-[754px]
+                    text-center">
+                        Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown
+                        printer took a galley of type and scrambled it to make a type specimen book. It has survived not only
+                        five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.
+                    </p>
+                    <Image
+                        src={"/images/cat9.png"}
+                        alt=""
+                        width={210}
+                        height={210}
+                    />
+                </div>
+                <div className="flex flex-col justify-between items-center" >
+                    <p className="text-gray-600 text-base leading-[2.3rem] text-start mt-[10px]">
+                        Lorem Ipsum is simply dummy text of the printing and typesetting industry. lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an
+                        unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting,
+                        remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop
+                        publishing software like Aldus PageMaker including versions of Lorem Ipsum.
+                    </p>
+                    <p className="text-gray-600 text-base leading-[2.3rem] text-start mt-[10px]">
+                        It is a long established fact that a reader will be distracted by the readable content of a page when looking at its layout. the point of using lorem Ipsum is that it has a
+                        more-or-less normal distribution of letters, as opposed to using
+                    </p>
+                    <p className="text-gray-600 text-base leading-[2.3rem] text-start mt-[10px]">
+                        Content here, content here', making it look like readable english. many desktop publishing packages and web page editors now use lorem Ipsum as their default model text,
+                        and a search for 'lorem ipsum' will uncover many web sites still in their infancy.
+                    </p>
+                    <p className="text-gray-600 text-base leading-[2.3rem] text-start mt-[10px]">
+                        Contrary to popular belief, lorem Ipsum is not simply random text. It has roots in a piece of classical latin literature from 45 BC, making it over 2000 years old. richard
+                        mcclintock, a latin professor at hampden-sydney college in virginia, looked up one of the more obscure latin words, consectetur, from a lorem Ipsum passage, and going through
+                    </p>
+                    <p className="text-gray-600 text-base leading-[2.3rem] text-start mt-[10px]">
+                        College in virginia, looked up one of the more obscure latin words, consectetur, from a lorem Ipsum passage, and going through the cites of the word in classical literature,
+                        discovered the undoubtable source. lorem Ipsum comes from sections 1.10.32 and 1.10.33 of 'de finibus bonorum et malorum' (the extremes of good and evil) by cicero, written in
+                    </p>
+                    <p className="text-gray-600 text-base leading-[2.3rem] text-start mt-[10px]">
+                        The standard chunk of lorem Ipsum used since the 1500s is reproduced below for those interested. sections 1.10.32 and 1.10.33 from "de finibus bonorum et malorum" by
+                        cicero are also reproduced in their
+                    </p>
+                </div>
+            </div>
+        </div>
     );
 }
- 
+
 export default About;
--- client/src/app/category/page.tsx
+++ client/src/app/category/page.tsx
@@ -1,5 +1,266 @@
+import Image from "next/image";
+import PlaceIcon from "@mui/icons-material/Place";
+import AddCircleOutlineIcon from "@mui/icons-material/AddCircleOutline";
 const Category = () => {
-  return <div className="">Category Page</div>;
+  return (
+    <div
+      className="
+    grid
+    justify-center
+    items-center
+    "
+    >
+      <div className="relative">
+        <h1
+          className="
+        text-white 
+        text-6xl
+        font-bold
+        absolute
+        ml-10
+        top-10
+        left-1/3
+        z-10
+        "
+        >
+          Category
+        </h1>
+        <Image
+          src={"/images/banner/banner-detail.png"}
+          alt=""
+          width={1200}
+          height={100}
+        ></Image>
+      </div>
+      <section
+        className="
+      mt-3
+      flex
+      justify-center
+      flex-col
+      items-center  "
+      >
+        <div
+          className="
+          bg-red-900 
+          h-40 
+          w-7/12
+          "
+        >
+          <div
+            className="
+            mt-3
+            ml-3
+            "
+          >
+            <h1
+              className="
+              text-white
+              text-3xl
+              "
+            >
+              <PlaceIcon
+                className="
+                text-green-500
+                text-4xl
+                "
+              />
+              Địa chỉ thanh toán
+            </h1>
+            <div
+              className="
+              ml-10
+              "
+            >
+              <h2>Name, SĐT:19001567</h2>
+              <p>áạldjaskdsjlaslkjdalsjdald</p>
+            </div>
+          </div>
+          <div
+            className="
+              mr-auto
+              ml-auto
+              "
+          >
+            <h2
+              className="
+              text-center 
+              text-xl
+              text-white
+              mt-5 
+              "
+            >
+              <AddCircleOutlineIcon
+                className="
+              text-green-500 
+              text-3xl
+              text-center
+              "
+              />
+              Thêm địa chỉ thanh toán mới
+            </h2>
+          </div>
+        </div>
+        <div
+          className="
+          w-7/12
+          flex
+          items-center
+          justify-between
+          p-2"
+        >
+          <Image
+            src={"/images/products/product1.png"}
+            alt=""
+            width={100}
+            height={100}
+          ></Image>
+          <p>name</p>
+          <p>quantity</p>
+          <p>12345678</p>
+        </div>
+        <div
+          className="
+        w-7/12
+        bg-cyan-200
+        "
+        >
+          <div
+            className="
+            flex
+            justify-between"
+          >
+            <div
+              className="
+          ml-5
+          mt-5
+          "
+            >
+              <div>
+                <input type="radio" name="" id="" className="" />
+                <label
+                  className="
+            ml-4 
+            text-xl
+            text-cyan-500"
+                  htmlFor=""
+                >
+                  : akjsdlakjd
+                </label>
+              </div>
+              <p
+                className="
+            text-cyan-500 
+            text-base 
+            ml-8
+            "
+              >
+                á;ldka;lkda;alksd;adkalksdjakljdaklj
+              </p>
+            </div>
+            <h1 className="
+            mt-5
+            mr-5   
+            text-2xl 
+            text-cyan-500"> 23456 VND</h1>
+          </div>
+          <div
+            className="
+            flex
+            justify-between
+            "
+          >
+            <div
+              className="
+          ml-5
+          mt-5
+          "
+            >
+              <div>
+                <input type="radio" name="" id="" className="" />
+                <label
+                  className="
+            ml-4 
+            text-xl
+            text-cyan-500"
+                  htmlFor=""
+                >
+                  : akjsdlakjd
+                </label>
+              </div>
+              <p
+                className="
+            text-cyan-500 
+            text-base 
+            ml-8
+            "
+              >
+                á;ldka;lkda;alksd;adkalksdjakljdaklj
+              </p>
+            </div>
+            <h1 className="
+            mt-5
+            mr-5   
+            text-2xl 
+            text-cyan-500"> 23456 VND</h1>
+          </div>
+          <div
+            className="
+            flex
+            justify-between"
+          >
+            <div
+              className="
+          ml-5
+          mt-5
+          "
+            >
+              <div>
+                <input type="radio" name="" id="" className="" />
+                <label
+                  className="
+            ml-4 
+            text-xl
+            text-cyan-500"
+                  htmlFor=""
+                >
+                  : akjsdlakjd
+                </label>
+              </div>
+              <p
+                className="
+            text-cyan-500 
+            text-base 
+            ml-8
+            "
+              >
+                á;ldka;lkda;alksd;adkalksdjakljdaklj
+              </p>
+            </div>
+            <h1 className="
+            mt-5
+            mr-5   
+            text-2xl 
+            text-cyan-500"> 23456 VND</h1>
+          </div>
+        </div>
+        <div className="
+        w-7/12
+        ">
+          <div className="
+          flex
+          justify-between
+          text-xl
+          m-5"> 
+            
+            <h1>gsdkjsashsda :</h1>
+            <p>12345656</p>
+
+          </div>
+        </div>
+      </section>
+    </div>
+  );
 };
 
 export default Category;
